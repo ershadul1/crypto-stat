37941e13cc7f68d824be74fbdf1cb86995ed61c0
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-03 23:29:53 +0600

    Fix import paths

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import App from './components/App/App';
-import Details from './components/Details/Details';
+import App from './containers/App/App';
+import Details from './containers/Details/Details';
 
 const Routes = () => (
   <BrowserRouter>
