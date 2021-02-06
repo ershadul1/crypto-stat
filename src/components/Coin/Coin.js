f5feff1b2d65a2bf0d42cfa4c16a6bdbc908ee5b
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-07 01:27:08 +0600

    Style Coin component

--- src/components/Coin/Coin.js
+++ src/components/Coin/Coin.js
@@ -3,30 +3,39 @@ import PropTypes from 'prop-types';
 import styles from './Coin.module.css';
 import roundNum from '../../helpers/roundNum';
 
-const Coin = ({ data, currency }) => (
-  <div>
-    <Link className={styles.container} to={`/details/${data.id}`}>
-      <div>
-        <img className={styles.logo} src={data.image} alt={`${data.name}-logo`} />
-        {data.name}
-      </div>
-      <div>
-        {`${currency.toUpperCase()} ${new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(data.current_price)}`}
-      </div>
-      <div>
-        {`${currency.toUpperCase()} ${new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(data.market_cap)}`}
-      </div>
-      <div>
-        {roundNum(data.price_change_percentage_24h)}
-        %
-      </div>
-      <div>
-        {roundNum(data.price_change_percentage_7d_in_currency)}
-        %
-      </div>
-    </Link>
-  </div>
-);
+const Coin = ({ data, currency }) => {
+  const percent24 = roundNum(data.price_change_percentage_24h);
+  const percent24Color = percent24 > 0 ? 'green' : 'red';
+  const percent7d = roundNum(data.price_change_percentage_7d_in_currency);
+  const percent7dColor = percent7d > 0 ? 'green' : 'red';
+  return (
+    <div>
+      <Link className={styles.container} to={`/details/${data.id}`}>
+        <div className={styles.cell}>
+          <img className={styles.logo} src={data.image} alt={`${data.name}-logo`} />
+          <p>{data.name}</p>
+        </div>
+        <div>
+          {`${currency.toUpperCase()} ${new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(data.current_price)}`}
+        </div>
+        <div>
+          {`${currency.toUpperCase()} ${new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(data.market_cap)}`}
+        </div>
+        <div>
+          {`${currency.toUpperCase()} ${new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(data.total_volume)}`}
+        </div>
+        <div id={percent24Color}>
+          {percent24}
+          %
+        </div>
+        <div id={percent7dColor}>
+          {roundNum(data.price_change_percentage_7d_in_currency)}
+          %
+        </div>
+      </Link>
+    </div>
+  );
+};
 
 Coin.propTypes = {
   data: PropTypes.shape({
@@ -35,6 +44,7 @@ Coin.propTypes = {
     image: PropTypes.string.isRequired,
     current_price: PropTypes.number.isRequired,
     market_cap: PropTypes.number.isRequired,
+    total_volume: PropTypes.number.isRequired,
     price_change_percentage_24h: PropTypes.number.isRequired,
     price_change_percentage_7d_in_currency: PropTypes.number.isRequired,
   }).isRequired,
