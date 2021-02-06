4214c03c6bb35f56ac65f3b4a3d4bc6b855d4e97
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-06 13:40:17 +0600

    Change App container name into Home comtainer

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import App from './containers/App/App';
+import Home from './containers/Home/Home';
 import Details from './containers/Details/Details';
+import Navbar from './components/Navbar/Navbar';
 
 const Routes = () => (
   <BrowserRouter>
+    <Navbar />
     <Switch>
-      <Route path="/" exact component={App} />
+      <Route path="/" exact component={Home} />
       <Route path="/details/:id" exact component={Details} />
     </Switch>
   </BrowserRouter>
