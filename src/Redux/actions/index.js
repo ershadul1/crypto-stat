6b693c031394318d564ad8c7e410d0c15db3ac33
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-06 13:42:33 +0600

    Add change filter action and reducer

--- src/Redux/actions/index.js
+++ src/Redux/actions/index.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 import * as actions from './actionTypes';
 
 const URL = 'https://api.coingecko.com/api/v3/coins';
-const fetchList = () => dispatch => {
-  axios.get(`${URL}/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false`)
+const fetchList = filter => dispatch => {
+  axios.get(`${URL}/markets?vs_currency=${filter.currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h%2C7d`)
     .then(response => {
+      console.log(response);
       dispatch({ type: actions.FETCH_LIST, payload: response });
     })
     .catch(error => dispatch({ type: actions.FETCH_LIST_ERROR, payload: error }));
@@ -18,4 +19,10 @@ const fetchDetails = id => dispatch => {
     .catch(error => dispatch({ type: actions.FETCH_DETAILS_ERROR, payload: error }));
 };
 
-export { fetchList, fetchDetails };
+const changeFilter = filter => dispatch => dispatch(
+  { type: actions.CHANGE_FILTER, payload: filter },
+);
+
+export {
+  fetchList, fetchDetails, changeFilter,
+};
