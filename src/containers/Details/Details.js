c5a040693b12e5acde75ff6eb3c6b46c4a78ae23
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-07 01:26:31 +0600

    Update Details component

--- src/containers/Details/Details.js
+++ src/containers/Details/Details.js
@@ -1,6 +1,10 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchDetails } from '../../Redux/actions/index';
+import Chart from '../../components/Chart/Chart';
+import * as styles from './Details.module.css';
+import roundNum from '../../helpers/roundNum';
 
 const Details = state => {
   const fetchCoinDetails = () => {
@@ -23,14 +27,49 @@ const Details = state => {
       </div>
     );
   }
-  const data = state.details.data.market_data.current_price.usd;
+
+  const percent24 = roundNum(state.details.data.market_data.price_change_percentage_24h);
+  const percent24Color = percent24 > 0 ? 'green' : 'red';
+
   return (
     <>
-      <h1>
-        Details
-      </h1>
-      <button type="button" onClick={fetchCoinDetails}>FETCH_DETAILS</button>
-      <p>{data}</p>
+      <div className={styles.container}>
+        <div className={styles.backcont}>
+          <h1>
+            {state.details.data.name}
+            <img
+              className={styles.logo}
+              src={state.details.data.image.small}
+              alt={state.details.data.name}
+            />
+            <div id={percent24Color}>
+              {percent24}
+              %
+            </div>
+          </h1>
+
+          <h1>
+            <Link className={styles.link} to="/">Back</Link>
+          </h1>
+        </div>
+
+        <p>
+          Rank #
+          {state.details.data.coingecko_rank}
+        </p>
+        <p>
+          {`Current Price: USD ${state.details.data.market_data.current_price.usd}`}
+        </p>
+        <p>
+          {`Liquidity Score: ${state.details.data.liquidity_score}`}
+        </p>
+        <div className={styles.chart}>
+          <Chart
+            className={styles.chart}
+            data={state.details.data.market_data.sparkline_7d.price}
+          />
+        </div>
+      </div>
     </>
   );
 };
