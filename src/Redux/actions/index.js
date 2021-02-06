606b899e3e880241efea41b8be871eec463de618
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-07 01:27:34 +0600

    Remove console statements

--- src/Redux/actions/index.js
+++ src/Redux/actions/index.js
@@ -5,14 +5,13 @@ const URL = 'https://api.coingecko.com/api/v3/coins';
 const fetchList = filter => dispatch => {
   axios.get(`${URL}/markets?vs_currency=${filter.currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h%2C7d`)
     .then(response => {
-      console.log(response);
       dispatch({ type: actions.FETCH_LIST, payload: response });
     })
     .catch(error => dispatch({ type: actions.FETCH_LIST_ERROR, payload: error }));
 };
 
 const fetchDetails = id => dispatch => {
-  axios.get(`${URL}/${id}`)
+  axios.get(`${URL}/${id}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=true`)
     .then(response => {
       dispatch({ type: actions.FETCH_DETAILS, payload: response });
     })
